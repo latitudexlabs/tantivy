852fcf4299da017c60c1561bf8750d5ec70d0a5a
spartan <developer@intellipins.com>
2026-01-20 22:57:35 +0530

    Updated intersection function seek

--- src/query/intersection.rs
+++ src/query/intersection.rs
@@ -109,44 +109,32 @@ impl<TDocSet: DocSet, TOtherDocSet: DocSet> DocSet for Intersection<TDocSet, TOt
     fn advance(&mut self) -> DocId {
         let (left, right) = (&mut self.left, &mut self.right);
         let mut candidate = left.advance();
-        if candidate == TERMINATED {
-            return TERMINATED;
-        }
 
-        loop {
+        'outer: loop {
             // In the first part we look for a document in the intersection
             // of the two rarest `DocSet` in the intersection.
 
             loop {
-                if right.seek_into_the_danger_zone(candidate) {
+                let right_doc = right.seek(candidate);
+                candidate = left.seek(right_doc);
+                if candidate == right_doc {
                     break;
-                }
-                let right_doc = right.doc();
-                // TODO: Think about which value would make sense here
-                // It depends on the DocSet implementation, when a seek would outweigh an advance.
-                if right_doc > candidate.wrapping_add(100) {
-                    candidate = left.seek(right_doc);
-                } else {
-                    candidate = left.advance();
-                }
-                if candidate == TERMINATED {
-                    return TERMINATED;
                 }
             }
 
             debug_assert_eq!(left.doc(), right.doc());
-            // test the remaining scorers
-            if self
-                .others
-                .iter_mut()
-                .all(|docset| docset.seek_into_the_danger_zone(candidate))
-            {
-                debug_assert_eq!(candidate, self.left.doc());
-                debug_assert_eq!(candidate, self.right.doc());
-                debug_assert!(self.others.iter().all(|docset| docset.doc() == candidate));
-                return candidate;
+            // test the remaining scorers;
+            for docset in self.others.iter_mut() {
+                let seek_doc = docset.seek(candidate);
+                if seek_doc > candidate {
+                    candidate = left.seek(seek_doc);
+                    continue 'outer;
+                }
             }
-            candidate = left.advance();
+            debug_assert_eq!(candidate, self.left.doc());
+            debug_assert_eq!(candidate, self.right.doc());
+            debug_assert!(self.others.iter().all(|docset| docset.doc() == candidate));
+            return candidate;
         }
     }
 
